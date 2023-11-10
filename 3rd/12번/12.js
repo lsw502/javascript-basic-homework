0ac392c87d1596d37b0c205f73c0fb2495f4f147
lsw502 <tkddnr1216@naver.com>
2023-11-10 18:08:41 +0900

    3회차 과제 완료

--- 3rd/12번/12.js
+++ 3rd/12번/12.js
@@ -5,10 +5,15 @@ const 투두 = {
 };
 
 // 여기에 코드를 작성해주세요.
-const newTodo = ?
+const newTodo = {...투두,
+  완료여부:true}
 
 console.log(투두)
 console.log(newTodo);
 // 결과
 // { id: 1, '할일': '8시기상', '완료여부': false }
 // { id: 1, '할일': '8시기상', '완료여부': true }
+
+
+// 다음 할 일의 완료여부를 true로 바꿔보세요. 
+// 단, 기존의 변수 “투두”의 값은 변경되면 안됩니다. (hint: spread operator)
