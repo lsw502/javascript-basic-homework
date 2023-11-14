34a1302eb73de1233f4df55ee1a83876e421f5da
lsw502 <tkddnr1216@naver.com>
2023-11-14 18:49:39 +0900

    4회차 과제 완료

--- 4th/5번/index.js
+++ 4th/5번/index.js
@@ -1,3 +1,30 @@
 // 여기에 1번 문제의 답을 작성하세요.
 
+const getProducts = async () => {
+    const response = await fetch('https://dummyjson.com/products');
+    const data = await response.json();
+    return data;
+};
 // 여기에 2번 문제의 답을 작성하세요.
+
+
+const getProducts = async () => {
+
+    const settings = {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            title: 'BMW Pencil',
+            /* other product data */
+        })
+    }
+    try {
+        const fetchResponse = await fetch(`https://dummyjson.com/products/add`, settings);
+        const data = await fetchResponse.json();
+        return data;
+    } catch (e) {
+        return e;
+    }
+
+}
+
